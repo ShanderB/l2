d21fbcf11b037a0fd6b20a281f3f2546689f402d
ShanderB <s.hander8910@hotmail.com>
2024-09-14 20:07:10 -0300

    Ajustado mock de games.

--- loja-de-jogos/src/app/services/game-service.ts
+++ loja-de-jogos/src/app/services/game-service.ts
@@ -5,10 +5,10 @@ import { Game } from '../entities/game';
   providedIn: 'root'
 })
 export class GameService {
-  private games = [
-    { name: 'Jogo 1', price: 59.99, boxSize: '30x30x10', description: 'Descrição do Jogo 1', coverImage: 'path/to/image1.jpg' },
-    { name: 'Jogo 2', price: 69.99, boxSize: '25x25x8', description: 'Descrição do Jogo 2', coverImage: 'path/to/image2.jpg' },
-    { name: 'Jogo 3', price: 79.99, boxSize: '35x35x12', description: 'Descrição do Jogo 3', coverImage: 'path/to/image3.jpg' }
+  private games: Game[] = [
+    { id: '1', name: 'Jogo 1', price: 59.99, boxHeight: 30, boxWidth: 30, boxLength: 10, description: 'Descrição do Jogo 1', coverImage: 'path/to/image1.jpg' },
+    { id: '2', name: 'Jogo 2', price: 69.99, boxHeight: 25, boxWidth: 25, boxLength: 8, description: 'Descrição do Jogo 2', coverImage: 'path/to/image2.jpg' },
+    { id: '3', name: 'Jogo 3', price: 79.99, boxHeight: 35, boxWidth: 35, boxLength: 12, description: 'Descrição do Jogo 3', coverImage: 'path/to/image3.jpg' }
   ];
 
   getGames(): Game[] {
