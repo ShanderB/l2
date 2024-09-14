5e4b46a393932975a7e049065475b8256f2d6607
ShanderB <s.hander8910@hotmail.com>
2024-09-14 20:31:40 -0300

    Adicionado toggle para items inseridos.

--- loja-de-jogos/src/app/components/main/main-page.component.ts
+++ loja-de-jogos/src/app/components/main/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from '../../services/game-service';
 import { Game } from '../../entities/game';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-main-page',
@@ -9,10 +10,67 @@ import { Game } from '../../entities/game';
 })
 export class MainPageComponent implements OnInit {
     games: Game[] = [];
+    gameForm: FormGroup;
+    isSelectionMode: boolean = true;
+    selectedGames: string[] = [];
 
-    constructor(private gameService: GameService) { }
+    constructor(private readonly gameService: GameService, private readonly fb: FormBuilder) {
+        this.gameForm = this.fb.group({
+            id: [''],
+            name: ['', Validators.required],
+            price: ['', Validators.required],
+            boxLength: ['', Validators.required],
+            boxWidth: ['', Validators.required],
+            boxHeight: ['', Validators.required],
+            description: [''],
+            coverImage: [''],
+        });
+    }
 
     ngOnInit(): void {
         this.games = this.gameService.getGames();
+        this.populateGameForm();
+
+    }
+
+    toggleSelectionMode(): void {
+        this.isSelectionMode = !this.isSelectionMode;
+    }
+
+    onCheckboxChange(game: Game, event: Event): void {
+
+        const inputElement = (event.target as HTMLInputElement)
+        const isChecked = inputElement.checked;
+
+        if (isChecked) {
+            this.selectedGames.push(game.id!)
+        } else {
+            const index = this.selectedGames.findIndex(x => x === game.id);
+            this.selectedGames.splice(index, 1);
+        }
+
+        console.log(this.selectedGames);
+
+    }
+
+    sendToBoxCalculator(): void {
+        // const selectedGames = this.selectedGamesForm.value.selectedGames;
+        // console.log('Dados enviados para a calculadora de caixa:', selectedGames);
+        // Lógica para enviar os dados para a calculadora de caixa
+    }
+
+    populateGameForm(): void {
+        this.games.forEach(game => {
+            this.gameForm.patchValue({
+                id: game.id,
+                name: game.name,
+                price: game.price,
+                boxLength: game.boxLength,
+                boxWidth: game.boxWidth,
+                boxHeight: game.boxHeight,
+                description: game.description,
+                coverImage: game.coverImage,
+            });
+        });
     }
 }
