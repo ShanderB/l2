b81bd40cc942621b674fd9e44481cf32daabe38d
ShanderB <s.hander8910@hotmail.com>
2024-09-14 21:47:25 -0300

    Ajuste para utilizar o gateway e utilizar gerenciamento de estado.

--- loja-de-jogos/src/app/gateways/game.gateway.ts
+++ loja-de-jogos/src/app/gateways/game.gateway.ts
@@ -6,8 +6,12 @@ import { Game } from '../entities/game';
     providedIn: 'root'
 })
 export class GameGateway {
-    private games: Game[] = [];
-
+    private games: Game[] = [
+        { id: '1', name: 'Jogo 1', price: 59.99, boxHeight: 30, boxWidth: 30, boxLength: 10, description: 'Descrição do Jogo 1', coverImage: 'path/to/image1.jpg' },
+        { id: '2', name: 'Jogo 2', price: 69.99, boxHeight: 25, boxWidth: 25, boxLength: 8, description: 'Descrição do Jogo 2', coverImage: 'path/to/image2.jpg' },
+        { id: '3', name: 'Jogo 3', price: 79.99, boxHeight: 35, boxWidth: 35, boxLength: 12, description: 'Descrição do Jogo 3', coverImage: 'path/to/image3.jpg' }
+      ];
+    
     addGame(game: Game): Observable<Game> {
         this.games.push(game);
         return of(game);
--- loja-de-jogos/src/app/services/game-service.ts
+++ loja-de-jogos/src/app/services/game-service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Game } from '../entities/game';
+import { GameUseCase } from '../use-cases/game/game.use-case';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
-  private games: Game[] = [
-    { id: '1', name: 'Jogo 1', price: 59.99, boxHeight: 30, boxWidth: 30, boxLength: 10, description: 'Descrição do Jogo 1', coverImage: 'path/to/image1.jpg' },
-    { id: '2', name: 'Jogo 2', price: 69.99, boxHeight: 25, boxWidth: 25, boxLength: 8, description: 'Descrição do Jogo 2', coverImage: 'path/to/image2.jpg' },
-    { id: '3', name: 'Jogo 3', price: 79.99, boxHeight: 35, boxWidth: 35, boxLength: 12, description: 'Descrição do Jogo 3', coverImage: 'path/to/image3.jpg' }
-  ];
+  constructor(private readonly gameUseCase: GameUseCase) { }
 
-  getGames(): Game[] {
-    return this.games;
+  addNewGame(game: Game): Observable<Game> {
+    return this.gameUseCase.addGame(game);
+  }
+
+  getAllGames(): Observable<Game[]> {
+    return this.gameUseCase.getGames();
   }
 }
