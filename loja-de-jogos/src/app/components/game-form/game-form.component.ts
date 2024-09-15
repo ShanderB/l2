8e04ce461d4c72f4ca9aaebe4f01d93f6de18af8
ShanderB <s.hander8910@hotmail.com>
2024-09-14 22:02:00 -0300

    Adicionado retorno para mostrar o resultado do cadastro. Adicionado validações para permitir apenas numeros.

--- loja-de-jogos/src/app/components/game-form/game-form.component.ts
+++ loja-de-jogos/src/app/components/game-form/game-form.component.ts
@@ -6,25 +6,51 @@ import { Game } from '../../entities/game';
 
 @Component({
     selector: 'app-game-form',
-    templateUrl: './game-form.component.html',
-    // styleUrls: ['./game-form.component.css']
+    templateUrl: './game-form.component.html'
 })
 export class GameFormComponent {
     gameForm: FormGroup;
+    message: string = '';
 
     constructor(private fb: FormBuilder, private gameUseCase: GameUseCase) {
         this.gameForm = this.fb.group({
             name: ['', Validators.required],
-            price: ['', Validators.required],
-            boxLength: ['', Validators.required],
-            boxWidth: ['', Validators.required],
-            boxHeight: ['', Validators.required],
+            price: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+            boxLength: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+            boxWidth: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+            boxHeight: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
             description: [''],
             coverImage: [''],
         });
     }
 
+    validateForm(): boolean {
+        if (this.gameForm.invalid) {
+            this.message = 'Por favor, preencha todos os campos corretamente.';
+            return false;
+        }
+        this.message = '';
+        return true;
+    }
+
     onSubmit() {
-        this.gameUseCase.addGame(this.gameForm.value).subscribe();
+        if (this.validateForm()) {
+            this.gameUseCase.addGame(this.gameForm.value).subscribe({
+                next: () => {
+                    this.message = 'Usuário cadastrado com sucesso!';
+                },
+                error: () => {
+                    this.message = 'Erro ao cadastrar o usuário. Preencha corretamente os campos.';
+                }
+            });
+        }
+    }
+
+    validateNumber(event: Event): void {
+        const input = event.target as HTMLInputElement;
+        input.value = input.value.replace(/[^0-9]/g, '');
+        if (input.value.startsWith('-')) {
+            input.value = input.value.substring(1);
+        }
     }
 }
