049bf6008e90261505a28b5cbb00d91846a0be5e
ShanderB <s.hander8910@hotmail.com>
2024-09-14 22:59:40 -0300

    Criado gerador automatico para imagens ao cadastrar um jogo.

--- src/app/gateways/game.gateway.ts
+++ src/app/gateways/game.gateway.ts
@@ -8,11 +8,17 @@ import { Game } from '../entities/game';
 export class GameGateway {
     private games: Game[] = [
         // { id: '1', name: 'Jogo 1', price: 59.99, boxHeight: 30, boxWidth: 30, boxLength: 10, description: 'Descrição do Jogo 1', coverImage: 'path/to/image1.jpg' },
-        { id: '2', name: 'Jogo 2', price: 69.99, boxHeight: 25, boxWidth: 25, boxLength: 8, description: 'Descrição do Jogo 2', coverImage: 'path/to/image2.jpg' },
-        { id: '3', name: 'Jogo 3', price: 79.99, boxHeight: 35, boxWidth: 35, boxLength: 12, description: 'Descrição do Jogo 3', coverImage: 'path/to/image3.jpg' }
-      ];
-    
+        { id: '2', name: 'Jogo 2', price: 69.99, boxHeight: 25, boxWidth: 25, boxLength: 8, description: 'Descrição do Jogo 2', coverImage: '/assets/1.jpg' },
+        { id: '3', name: 'Jogo 3', price: 79.99, boxHeight: 35, boxWidth: 35, boxLength: 12, description: 'Descrição do Jogo 3', coverImage: '/assets/2.jpg' }
+    ];
+
+    private getRandomImage(): string {
+        const randomIndex = Math.floor(Math.random() * 5) + 1;
+        return `/assets/${randomIndex}.jpg`;
+    }
+
     addGame(game: Game): Observable<Game> {
+        game.coverImage = this.getRandomImage();
         this.games.push(game);
         return of(game);
     }
